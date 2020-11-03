1886d12af1726224dc3d6b9c958846b7edbc4645
NP <navdeep.puri@gmail.com>
2020-11-03 07:28:25 -0500

    added containers and routes

---
+++ src/containers/Portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>My Portfolio page</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
