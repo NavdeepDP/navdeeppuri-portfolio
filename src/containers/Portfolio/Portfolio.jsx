603b431516cf78a3656c1b98bafb8282938201f4
NP <navdeep.puri@gmail.com>
2020-11-04 10:02:03 -0500

    added projects in portfolio

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -3,6 +3,12 @@ import Project from "../../components/Project/Project";
 
 import StraightSets from "./../images/straightSets-1.gif";
 import WeatherDashboard from "./../images/Weather-Dashboard.gif";
+import EmployeeDirectory from "./../images/emp-dir.gif"
+import CodeQuiz from "./../images/Coding-Quiz.gif"
+import TeamGenerator from "./../images/team.png"
+import Adventure from "./../images/FindYourAdventure1.png"
+
+
 
 const Portfolio = () => {
   return (
@@ -14,6 +20,10 @@ const Portfolio = () => {
           </div>
           <Project title="Straight Sets" image={StraightSets} />
           <Project title="Weather Dashboard" image={WeatherDashboard} />
+          <Project title="Employee Directory" image={EmployeeDirectory} />
+          <Project title="Code Quiz" image={CodeQuiz} />
+          <Project title="Team Generator" image={TeamGenerator} />
+          <Project title="Find Your Adventure" image={Adventure} />
         </div>
       </div>
     </div>
